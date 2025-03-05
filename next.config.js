450163a6c29dcf448342093fc06c03b377986b68
dhruvparmar2310 <dhanparmar23@gmail.com>
2025-03-05 22:46:41 +0530

    [feat]: added new interviews

--- next.config.js
+++ next.config.js
@@ -22,7 +22,8 @@ const nextConfig = {
     LOCALHOST: 'http://localhost:3000',
     DEPLOY: 'https://physiozine.vercel.app/',
     CASHFEE_API_KEY: '7253322be58d82f5ff1e61fe52233527',
-    CASHFEE_SECKRET_KEY: 'cfsk_ma_prod_c501199e41230c036c8a78a41cfe16b9_ab125276'
+    CASHFEE_SECKRET_KEY: 'cfsk_ma_prod_c501199e41230c036c8a78a41cfe16b9_ab125276',
+    ZENODO_SECKRET_KEY: '4krGrmKnTPkGLNZv4kx1PHNN18Yewhr9EwvNF1vmp2ro7VKDO4c2V6KMRMvP'
   },
 }
 
